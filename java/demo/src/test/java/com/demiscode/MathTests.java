702560596bda0840bde55a5f883693ec3d017c3d
Demis Getachew <demisgech@gmail.com>
2025-04-11 20:33:45 +0300

    Setup and tearDown

--- java/demo/src/test/java/com/demiscode/MathTests.java
+++ java/demo/src/test/java/com/demiscode/MathTests.java
@@ -2,16 +2,48 @@ package com.demiscode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Math Tests")
 public class MathTests {
 
+    private Math math;
+
+    // Setup -> before test execution
+    @BeforeAll
+    public static void InitAll() {
+        System.out.println("Before all test");
+        System.out.println("Applied on Static Methods");
+    }
+
+    @BeforeEach
+    public void init() {
+        math = new Math();
+        System.out.println("Before Each Test");
+        System.out.println("Applied on Instance fields");
+    }
+
+    // TearDown -> after test execution
+    @AfterEach
+    public void tearDown() {
+        math = null;
+        System.out.println("Afeter Each Test");
+        System.out.println("Applied on Instance fields");
+    }
+
+    @AfterAll
+    static public void killAll() {
+        System.out.println("After All Tests");
+        System.out.println("Applide on static fieds");
+    }
+
     @Test
     public void addWhenCalledShouldReturnTheSumOfArguments() {
-        var math = new Math();
-
         var result = math.add(10, 20);
 
         assertEquals(30, result);
@@ -19,8 +51,6 @@ public class MathTests {
 
     @Test
     public void maxWhenCalledShouldReturnTheFirstArgumentIfGereater() {
-        var math = new Math();
-
         var result = math.max(20, 10);
 
         assertEquals(20, result);
@@ -28,8 +58,6 @@ public class MathTests {
 
     @Test
     public void maxWhenCalledShouldReturnTheSecondArgumentIfGreater() {
-        var math = new Math();
-
         var result = math.max(10, 20);
 
         assertEquals(20, result);
@@ -37,10 +65,9 @@ public class MathTests {
 
     @Test
     public void maxWhenCalledShouldReturnArgumentIfTheyEqual() {
-        var math = new Math();
-
         var result = math.max(10, 10);
 
         assertEquals(10, result);
     }
+
 }
